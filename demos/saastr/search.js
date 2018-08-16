2630e6d1742b99d0793a0f2d253db941b3b979de
Pixelastic <tim@pixelastic.com>
2018-08-16 20:02:19 +0200

    Updates

---
+++ demos/saastr/search.js
@@ -0,0 +1,57 @@
+"use strict";
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+const indexName = 'saastr';
+const apiKey = '6e7463a1c6bd72ccbdf5caf750d17cbd';
+const placeholder = null;
+const searchParameters = {};
+const search = instantsearch({
+  appId: '8J7GPSC0XN',
+  apiKey,
+  indexName,
+  routing: true,
+  searchParameters: _objectSpread({
+    hitsPerPage: 21,
+    facetingAfterDistinct: true
+  }, searchParameters)
+});
+search.on('render', () => {
+  const pageLoadElement = document.getElementById('firstRender');
+
+  if (!pageLoadElement) {
+    setTimeout(() => {
+      const span = document.createElement('span');
+      span.setAttribute('id', 'firstRender');
+      document.body.appendChild(span);
+    }, 1000);
+  }
+});
+search.addWidget(instantsearch.widgets.searchBox({
+  container: '#searchbox',
+  placeholder: placeholder || 'Search inside all videos',
+  poweredBy: false,
+  wrapInput: false,
+  loadingIndicator: false
+}));
+search.addWidget(instantsearch.widgets.menu({
+  container: '#years',
+  attributeName: 'conference.year',
+  sortBy: ['name:desc']
+}));
+search.addWidget(instantsearch.widgets.hits({
+  container: '#hits',
+  templates: {
+    item: talksearch.hit,
+    empty: 'Sorry, no results found'
+  }
+}));
+search.addWidget(instantsearch.widgets.pagination({
+  container: '#pagination',
+  maxPages: 20,
+  scrollTo: false,
+  showFirstLast: false
+}));
+search.start();
